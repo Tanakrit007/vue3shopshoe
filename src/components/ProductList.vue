388afd990cc83ccab7edf223b508e377aab57503
Wuttha <Wuttha@webmail.npru.ac.th>
2024-10-22 10:10:33 +0700

    update

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="product-list">
     <div v-for="product in products" :key="product.id" class="product-item">
-      <img :src="product.src" :alt="product.name" />
+      <div class="container-image">
+        <img :src="`/vue3shopshoe/src/images/${product.src}`" :alt="product.name" />
+      </div>
       <h3>{{ product.name }}</h3>
       <p>{{ formatCurrency(product.price) }}</p>
       <button @click="viewProduct(product.id)">ดูรายละเอียด</button>
@@ -16,31 +18,31 @@ export default {
   data() {
     return {
       products: [
-        { id: 1, name: "CORZA", price: 150, src: "@/images/CORZA.jpg" },
-        { id: 2, name: "CORRELATE", price: 800, src: "@/images/CORRELATE.svg" },
+        { id: 1, name: "CORZA", price: 150, src: "CORZA.jpg" },
+        { id: 2, name: "CORRELATE", price: 800, src: "CORRELATE.svg" },
         {
           id: 3,
           name: "EXPERIENCE-RN-11",
           price: 2500,
-          src: "/images/EXPERIENCE-RN-11.svg",
+          src: "EXPERIENCE-RN-11.svg",
         },
         {
           id: 4,
           name: "VAPORMAX 2021 FK",
           price: 2000,
-          src: "/images/VAPORMAX 2021 FK.jpg",
+          src: "VAPORMAX 2021 FK.jpg",
         },
         {
           id: 5,
           name: "WAFFLE ONE",
           price: 4500,
-          src: "/images/WAFFLE ONE.jpg",
+          src: "WAFFLE ONE.jpg",
         },
         {
           id: 6,
           name: "ONDECK FLIP FLOP",
           price: 3000,
-          src: "/images/ONDECK FLIP FLOP.jpg",
+          src: "ONDECK FLIP FLOP.jpg",
         },
       ],
     };
@@ -70,5 +72,14 @@ export default {
   padding: 16px;
   width: 200px;
   text-align: center;
+}
+.container-image{
+  width: 200px;
+  height: 200px;
+}
+.container-image>img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
